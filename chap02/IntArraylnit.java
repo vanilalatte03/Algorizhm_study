747067b4ebaee1637377d3cb0a958dafd957e183
JH <jiho4890@gmail.com>
2024-08-25 23:11:38 +0900

    update

---
+++ chap02/IntArraylnit.java
@@ -0,0 +1,10 @@
+package chap02;
+
+public class IntArraylnit {
+  public static void main(String[] args) {
+    int[] a = {1, 2, 3, 4, 5};
+
+    for (int i = 0; i < a.length; i++)
+    System.out.println("a[" + i + "]= " + a[i]);
+  }
+}
